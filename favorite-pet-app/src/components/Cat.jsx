39ee327b791fb3db666efca3903198737bacf27e
MaryHooker <mc020791@yahoo.com>
2020-03-19 13:06:09 -0500

    styling

--- favorite-pet-app/src/components/Cat.jsx
+++ favorite-pet-app/src/components/Cat.jsx
@@ -8,7 +8,7 @@ class Cat extends Component {
     render() { 
         return ( 
             <div>
-                <h2>You're a cat person!</h2>
+                <h2>You're a <span className='catDog'>cat</span> person!</h2>
                 <img src="https://cdn.vox-cdn.com/thumbor/-XrOUcsL2DeFQd6DSColBnqc2qg=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19424316/EKo3U_qXkAEK1Fz.jpeg" alt="cat" className='cat'/>
             </div>
          );
--- favorite-pet-app/src/components/Dog.jsx
+++ favorite-pet-app/src/components/Dog.jsx
@@ -8,7 +8,7 @@ class Dog extends Component {
     render() { 
         return ( 
             <div>
-              <h2>You're a dog person!</h2>
+              <h2>You're a <span className='catDog'>dog</span> person!</h2>
               <img src="https://static.standard.co.uk/s3fs-public/thumbnails/image/2019/03/15/17/pixel-dogsofinstagram-3-15-19.jpg" alt="dog" className='dog'/>
             </div>
          );
